d33b68d2a1c6002703e52ff4517e4e94e5bc198c
riadhhamoudi <riadhhamoudi11@gmail.com>
2024-03-08 01:02:36 +0100

    done

--- Front-end/project/src/App.js
+++ Front-end/project/src/App.js
@@ -1,11 +1,45 @@
 import './App.css';
+import React from 'react';
+import  { useState } from 'react';
+
 
 function App() {
   return (
     <div className="App">
-      <h3>hello </h3>
+
+
+
+      <div className="container">
+        <div className="form-box">
+
+          <div className="header-form">
+          </div>
+          <div className="body-form">
+           <form>
+              <div className="input-group ">
+                <input type="email" className="form-control" placeholder="enter your email address"   required />
+              </div>
+              <div className="input-group ">
+
+                  <input type="password" className="form-control" placeholder="Password" required />
+              </div>
+
+              <div className='btncenteeer'>
+                <button type="submit" className="btn">LOGIN</button>
+              </div>
+              <div className="message">
+               <div><input type="checkbox" /> Remember ME</div>
+                <div><a href="#">Forgot your password</a></div>
+              </div>
+            </form>
+
+          </div>
+        </div>
+       </div>
     </div>
   );
 }
+
+
 
 export default App;
